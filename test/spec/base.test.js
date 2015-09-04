8dd93cb618b00554b2230e314681ce9071ddd6e4
Thanasis Polychronakis <thanpolas@gmail.com>
2015-09-04 12:39:02 +0300

    prep tests

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -4,6 +4,8 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var netcheck = require('../..');
+
 describe('Base API Surface', function() {
   it('should do what...', function(){
      // test!
